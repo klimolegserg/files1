2b095395a49f8ede718d4d1a4e315518314b8673
Oleg Klimenko <klimmail@rambler.ru>
2023-12-04 12:44:50 +0300

    changes

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
-cook_book = {}
-with open('data.txt', 'rt', encoding='utf-8') as fl:
-    for line in fl:
-        new_name = fl.readline().strip()
-        print(new_name)
-        count = int(fl.readline())
-        print(count)
-        dish_ing = []
-        for x in range(count):
-            cook = fl.readline().split()[::2]
-            print(cook)
-            ingredient_name, quantity, measure = cook
-            dish_ing.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-            empty = fl.readline()
-            cook_book['new_name'] = dish_ing
-    print(cook_book)
+def my_cook_book():
+    with open('data.txt', encoding='utf-8') as file:
+        cook_book = {}
+        for line in file.read().split('\n\n'):
+            name, _, *args = line.split('\n')
+            cook_li = []
+            for arg in args:
+                ingredient_name, quantity, measure = map(lambda x: int(x) if x.isdigit() else x, arg.split(' | '))
+                cook_li.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+                cook_book[name] = cook_li
+                int(quantity)
+    return cook_book
 
+
+my_cook_book()
+
+
+def get_shop_list_by_dishes(dishes, person_count: int):
+    result = {}
+    cook_book = {
+        'Омлет': [
+            {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+            {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+            {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+        ],
+        'Утка по-пекински': [
+            {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+            {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+            {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+            {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+        ],
+        'Запеченный картофель': [
+            {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+            {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+            {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+        ]
+    }
+    for dish in dishes:
+        if dish in cook_book:
+            for consist in cook_book[dish]:
+                if consist['ingredient_name'] in result:
+                    result[consist['ingredient_name']]['quantity'] += consist['quantity'] * person_count
+                else:
+                    result[consist['ingredient_name']] = {'measure': consist['measure'],'quantity': (consist['quantity'] * person_count)}
+        else:
+            print('Такого блюда нет в меню')
+
+    return result
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
